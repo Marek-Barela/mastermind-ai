a17c6601359304e91684c2a53e704bafe65b9e69
Marek Bareła <marek.barela@wp.pl>
2023-08-08 14:26:08 +0200

    Created sidebar and navbar components

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Navbar from "@/components/features/Navbar";
+import Sidebar from "@/components/features/Sidebar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -8,8 +10,9 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-zinc-900">
-        sidebar component
+        <Sidebar />
       </div>
+      <Navbar />
       <main className="md:pl-72">{children}</main>
     </div>
   );
---
+++ components/features/Sidebar.tsx
@@ -0,0 +1,90 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
+
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const routes = [
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    href: "/conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    href: "/image",
+    icon: ImageIcon,
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    href: "/video",
+    icon: VideoIcon,
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    href: "/music",
+    icon: Music,
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    href: "/code",
+    icon: Code,
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: Settings,
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-zinc-900 text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-12 h-12 mr-4">
+            <Image fill alt="Logo" src="/next.svg" />
+          </div>
+          <h1 className={cn("text-xl font-bold", montserrat.className)}>Mastermind</h1>
+        </Link>
+        <div className="space-y-1">
+          {routes.map(route => (
+            <Link
+              key={route.href}
+              href={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
